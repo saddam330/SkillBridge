5c12efa94ad83861801c884be37e9a9d98000f51
dzhansu bashara <cansubasari@gmail.com>
2025-05-27 18:18:17 +0100

    index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,4 +20,15 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  def apply
+    @project = Project.find(params[:id])
+    @application = @project.applications.new(user: current_user)
+
+    if @application.save
+      redirect_to @project_path, notice: 'Application was successfully created.'
+    else
+      redirect_to @project, alert: 'Failed to apply for the project.'
+    end
+  end
 end
