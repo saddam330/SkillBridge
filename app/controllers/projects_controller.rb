9652054a372973cd9332e4ff19625aa6ddffdb92
Saddam Hussain saddam5533@hotmail.com <saddam5533@hotmail.com>
2025-05-27 16:44:59 +0100

    projects new page is ready

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,17 +7,31 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def edit
   end
 
   def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project posted successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:project_title, :project_description, :job_title, :job_description,
+    :requirements, :duration, :category, :closing_date, :start_date, :location)
   end
 end
