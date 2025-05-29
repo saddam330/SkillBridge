90f482e1f70827551fa48b1913747c75e85def80
Dragan Ajvazovic <204484118+Dragan-London@users.noreply.github.com>
2025-05-29 17:24:06 +0100

    Added footer and created aply page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project, only: [:new, :create]
+
   def index
   end
 
@@ -6,12 +9,32 @@ class ApplicationsController < ApplicationController
   end
 
   def new
-
+    @application = Application.new
+    @user = User.all
   end
 
   def create
+    @application = @project.applications.new(application_params)
+    @application.user = current_user
+    @application.status = "pending"
+
+    if @application.save
+      redirect_to applications_path, notice: "Your application successfully submitted!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def application_params
+    params.require(:application).permit(:status, :cover_letter)
   end
 end
