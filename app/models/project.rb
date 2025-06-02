ad155dd63833a5bec6393d0225a35b0725e4d0c3
Saddam Hussain saddam5533@hotmail.com <saddam5533@hotmail.com>
2025-06-02 14:57:08 +0100

    fixing bugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :applications, dependent: :destroy
   has_many :users, through: :applications
+  validate :closing_date_before_start_date
 
   validates :project_title, :project_description, :job_title, :job_description, :duration,
             :requirements, :closing_date, :start_date, :location, presence: true
@@ -11,6 +12,15 @@ class Project < ApplicationRecord
   pg_search_scope :search_by_title_and_description,
     against: [:project_title, :project_description],
     using: {
-      tsearch: { prefix: true } 
+      tsearch: { prefix: true }
     }
+
+
+    private
+
+    def closing_date_before_start_date
+      if closing_date.present? && start_date.present? && closing_date > start_date
+        errors.add(:closing_date, "must be before the project start date.")
+      end
+    end
 end
--- db/migrate/20250528162426_change_column_name_for_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNameForUsers < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :users, :employee, :employer
-
-  end
-end
