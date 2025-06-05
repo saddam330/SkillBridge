a11a830e2794ca4a9685ee90cc3ac0dfaf7a2c1d
Hamja Sultan <sultanhamja597@gmail.com>
2025-06-05 14:06:24 +0100

    fixed styles and home banner link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @feedbacks = Feedback.order("RANDOM()").limit(3)
+    @feedbacks = Feedback.where("rating > 4").limit(3)
     @projects = Project.order(created_at: :desc).limit(3)
   end
 end
